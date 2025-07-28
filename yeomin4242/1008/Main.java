49eaa5acfdee0efac7e59270ca419e3adeca6349
yeomin4242 <whereismysejong@naver.com>
2025-07-28 14:40:51 +0900

    Test: Append new mock file

---
+++ yeomin4242/1008/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double x = Integer.parseInt(st.nextToken());
+        double y = Integer.parseInt(st.nextToken());
+        double sum = x / y;
+
+        System.out.printf("%.9f",sum);
+
+        return ;
+    }
+}
