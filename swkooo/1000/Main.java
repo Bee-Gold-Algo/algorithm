afc7918b339e9d876d55eceb46729ed13ee6dce5
구서원 <seowon710@naver.com>
2025-07-28 17:12:59 +0900

    week 1 1000번 문제 제출

---
+++ swkooo/1000/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+	
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		// a + b를 합하여 출력
+		System.out.println(a + b);
+	}
+	
+}
